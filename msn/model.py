9d39e7b1cb09045f2c7d595be8d21194b11f39e5
Bingchen Gong <gongbingchen@gmail.com>
2021-09-12 18:35:06 +0000

    dataloader working

--- msn/model.py
+++ msn/model.py
@@ -1,19 +1,19 @@
 from __future__ import print_function
+
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.nn.parallel
 import torch.utils.data
 from torch.autograd import Variable
-import numpy as np
-import torch.nn.functional as F
-import sys
-sys.path.append("./expansion_penalty/")
-import expansion_penalty_module as expansion
-sys.path.append("./MDS/")
-import MDS_module
+
+from .MDS.MDS_module import minimum_density_sample, gather_operation
+from .expansion_penalty.expansion_penalty_module import expansionPenaltyModule
+
 
 class STN3d(nn.Module):
-    def __init__(self, num_points = 2500):
+    def __init__(self, num_points=2500):
         super(STN3d, self).__init__()
         self.num_points = num_points
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
@@ -29,24 +29,26 @@ class STN3d(nn.Module):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x,_ = torch.max(x, 2)
+        x, _ = torch.max(x, 2)
         x = x.view(-1, 1024)
 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
 
-        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        iden = Variable(torch.from_numpy(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32))).view(1, 9).repeat(
+            batchsize, 1)
         if x.is_cuda:
             iden = iden.cuda()
         x = x + iden
         x = x.view(-1, 3, 3)
         return x
 
+
 class PointNetfeat(nn.Module):
-    def __init__(self, num_points = 8192, global_feat = True):
+    def __init__(self, num_points=8192, global_feat=True):
         super(PointNetfeat, self).__init__()
-        self.stn = STN3d(num_points = num_points)
+        self.stn = STN3d(num_points=num_points)
         self.conv1 = torch.nn.Conv1d(3, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
@@ -57,28 +59,30 @@ class PointNetfeat(nn.Module):
 
         self.num_points = num_points
         self.global_feat = global_feat
+
     def forward(self, x):
         batchsize = x.size()[0]
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.bn3(self.conv3(x))
-        x,_ = torch.max(x, 2)
+        x, _ = torch.max(x, 2)
         x = x.view(-1, 1024)
         return x
 
+
 class PointGenCon(nn.Module):
-    def __init__(self, bottleneck_size = 8192):
+    def __init__(self, bottleneck_size=8192):
         self.bottleneck_size = bottleneck_size
         super(PointGenCon, self).__init__()
         self.conv1 = torch.nn.Conv1d(self.bottleneck_size, self.bottleneck_size, 1)
-        self.conv2 = torch.nn.Conv1d(self.bottleneck_size, self.bottleneck_size//2, 1)
-        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//2, self.bottleneck_size//4, 1)
-        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//4, 3, 1)
+        self.conv2 = torch.nn.Conv1d(self.bottleneck_size, self.bottleneck_size // 2, 1)
+        self.conv3 = torch.nn.Conv1d(self.bottleneck_size // 2, self.bottleneck_size // 4, 1)
+        self.conv4 = torch.nn.Conv1d(self.bottleneck_size // 4, 3, 1)
 
         self.th = nn.Tanh()
         self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size)
-        self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2)
-        self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4)
+        self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size // 2)
+        self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size // 4)
 
     def forward(self, x):
         batchsize = x.size()[0]
@@ -87,6 +91,7 @@ class PointGenCon(nn.Module):
         x = F.relu(self.bn3(self.conv3(x)))
         x = self.th(self.conv4(x))
         return x
+
 
 class PointNetRes(nn.Module):
     def __init__(self):
@@ -98,7 +103,6 @@ class PointNetRes(nn.Module):
         self.conv5 = torch.nn.Conv1d(512, 256, 1)
         self.conv6 = torch.nn.Conv1d(256, 128, 1)
         self.conv7 = torch.nn.Conv1d(128, 3, 1)
-
 
         self.bn1 = torch.nn.BatchNorm1d(64)
         self.bn2 = torch.nn.BatchNorm1d(128)
@@ -116,7 +120,7 @@ class PointNetRes(nn.Module):
         pointfeat = x
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.bn3(self.conv3(x))
-        x,_ = torch.max(x, 2)
+        x, _ = torch.max(x, 2)
         x = x.view(-1, 1024)
         x = x.view(-1, 1024, 1).repeat(1, 1, npoints)
         x = torch.cat([x, pointfeat], 1)
@@ -126,48 +130,51 @@ class PointNetRes(nn.Module):
         x = self.th(self.conv7(x))
         return x
 
+
 class MSN(nn.Module):
-    def __init__(self, num_points = 8192, bottleneck_size = 1024, n_primitives = 16):
+    def __init__(self, num_points=8192, bottleneck_size=1024, n_primitives=16):
         super(MSN, self).__init__()
         self.num_points = num_points
         self.bottleneck_size = bottleneck_size
         self.n_primitives = n_primitives
         self.encoder = nn.Sequential(
-        PointNetfeat(num_points, global_feat=True),
-        nn.Linear(1024, self.bottleneck_size),
-        nn.BatchNorm1d(self.bottleneck_size),
-        nn.ReLU()
+            PointNetfeat(num_points, global_feat=True),
+            nn.Linear(1024, self.bottleneck_size),
+            nn.BatchNorm1d(self.bottleneck_size),
+            nn.ReLU()
         )
-        self.decoder = nn.ModuleList([PointGenCon(bottleneck_size = 2 +self.bottleneck_size) for i in range(0,self.n_primitives)])
+        self.decoder = nn.ModuleList(
+            [PointGenCon(bottleneck_size=2 + self.bottleneck_size) for i in range(0, self.n_primitives)])
         self.res = PointNetRes()
-        self.expansion = expansion.expansionPenaltyModule()
+        self.expansion = expansionPenaltyModule()
 
     def forward(self, x):
         partial = x
         x = self.encoder(x)
         outs = []
-        for i in range(0,self.n_primitives):
-            rand_grid = Variable(torch.cuda.FloatTensor(x.size(0),2,self.num_points//self.n_primitives))
-            rand_grid.data.uniform_(0,1)
-            y = x.unsqueeze(2).expand(x.size(0),x.size(1), rand_grid.size(2)).contiguous()
-            y = torch.cat( (rand_grid, y), 1).contiguous()
+        for i in range(0, self.n_primitives):
+            rand_grid = Variable(torch.cuda.FloatTensor(x.size(0), 2, self.num_points // self.n_primitives))
+            rand_grid.data.uniform_(0, 1)
+            y = x.unsqueeze(2).expand(x.size(0), x.size(1), rand_grid.size(2)).contiguous()
+            y = torch.cat((rand_grid, y), 1).contiguous()
             outs.append(self.decoder[i](y))
 
-        outs = torch.cat(outs,2).contiguous() 
-        out1 = outs.transpose(1, 2).contiguous() 
-        
-        dist, _, mean_mst_dis = self.expansion(out1, self.num_points//self.n_primitives, 1.5)
+        outs = torch.cat(outs, 2).contiguous()
+        out1 = outs.transpose(1, 2).contiguous()
+
+        dist, _, mean_mst_dis = self.expansion(out1, self.num_points // self.n_primitives, 1.5)
         loss_mst = torch.mean(dist)
 
         id0 = torch.zeros(outs.shape[0], 1, outs.shape[2]).cuda().contiguous()
-        outs = torch.cat( (outs, id0), 1)
+        outs = torch.cat((outs, id0), 1)
         id1 = torch.ones(partial.shape[0], 1, partial.shape[2]).cuda().contiguous()
-        partial = torch.cat( (partial, id1), 1)
-        xx = torch.cat( (outs, partial), 2)
+        partial = torch.cat((partial, id1), 1)
+        xx = torch.cat((outs, partial), 2)
 
-        resampled_idx = MDS_module.minimum_density_sample(xx[:, 0:3, :].transpose(1, 2).contiguous(), out1.shape[1], mean_mst_dis) 
-        xx = MDS_module.gather_operation(xx, resampled_idx)
+        resampled_idx = minimum_density_sample(xx[:, 0:3, :].transpose(1, 2).contiguous(), out1.shape[1],
+                                               mean_mst_dis)
+        xx = gather_operation(xx, resampled_idx)
         delta = self.res(xx)
-        xx = xx[:, 0:3, :] 
-        out2 = (xx + delta).transpose(2,1).contiguous()  
+        xx = xx[:, 0:3, :]
+        out2 = (xx + delta).transpose(2, 1).contiguous()
         return out1, out2, loss_mst
--- recon.py
+++ recon.py
@@ -1,15 +1,41 @@
 import argparse
 from pathlib import Path
 
+import torch
+from torch.utils.data import DataLoader
+
 from configs.config_utils import CONFIG
 from configs.scannet_config import ScannetConfig
-from dataloader import ISCNet_ScanNet
+from dataloader import ISCNet_ScanNet, collate_fn, my_worker_init_fn
+from msn.model import MSN
+from msn.utils import weights_init
 from net_utils.utils import initiate_environment
 
 
-def run():
-    test_loader = ISCNet_ScanNet(cfg, mode='test')
-    for iter, data in enumerate(test_loader):
+def run(opt):
+    network = MSN(num_points=opt.num_points, n_primitives=opt.n_primitives)
+    network.cuda()
+    network.apply(weights_init)
+
+    if opt.model != '':
+        network.load_state_dict(torch.load(opt.model))
+        print("Previous weight loaded ")
+
+    network.eval()
+
+    dataset = ISCNet_ScanNet(cfg, mode='test')
+    dataloader = DataLoader(dataset=dataset,
+                            num_workers=cfg.config['device']['num_workers'],
+                            batch_size=1,
+                            shuffle=False,
+                            collate_fn=collate_fn,
+                            worker_init_fn=my_worker_init_fn)
+
+    for cur_iter, data in enumerate(dataloader):
+        for idx in torch.nonzero(data['box_label_mask'].squeeze(0)):
+            print(data)
+
+        output1, output2, expansion_penalty = network(partial.transpose(2, 1).contiguous())
         print(data)
 
 
@@ -22,6 +48,10 @@ def parse_args():
     parser.add_argument('--mode', type=str, default='train', help='train, test or demo.')
     parser.add_argument('--demo_path', type=Path, default=base_dir / 'demo' / 'inputs' / 'scene0549_00.off',
                         help='Please specify the demo path.')
+    parser.add_argument('--num_points', type=int, default=8192, help='number of points')
+    parser.add_argument('--n_primitives', type=int, default=16, help='number of primitives in the atlas')
+    parser.add_argument('--model', type=Path, default=Path('trained_model', 'network.pth'),
+                        help='optional reload model path')
     return parser.parse_args()
 
 
@@ -36,4 +66,4 @@ if __name__ == '__main__':
     cfg.log_string(cfg.config)
     cfg.write_config()
 
-    run()
+    run(args)
